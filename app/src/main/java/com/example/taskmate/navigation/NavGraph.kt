9a5c7abafb50b6868eca217a92a37847cda9b7f8
Atreyan Raman <146109597+atreyanr@users.noreply.github.com>
2025-06-20 17:31:39 -0500

    Update NavGraph.kt

--- app/src/main/java/com/example/taskmate/navigation/NavGraph.kt
+++ app/src/main/java/com/example/taskmate/navigation/NavGraph.kt
@@ -16,22 +16,23 @@ import com.example.taskmate.data.SettingsDataStore
 import com.example.taskmate.viewmodel.TaskViewModel
 
 @Composable
-fun NavGraph(taskViewModel: TaskViewModel,
-             settingsStore: SettingsDataStore,
-             startDestination: String = "home") {
+fun NavGraph(
+    taskViewModel: TaskViewModel,
+    settingsStore: SettingsDataStore,
+    startDestination: String = "home"
+) {
     val navController: NavHostController = rememberNavController()
     Scaffold(
         bottomBar = {
             BottomNavBar(navController)
         }
-    )
-    { innerPadding ->
+    ) { innerPadding ->
         NavHost(
             navController = navController,
             startDestination = startDestination,
             modifier = Modifier.padding(innerPadding)
         ) {
-            composable("home") { HomeScreen(taskViewModel)}
+            composable("home") { HomeScreen(taskViewModel) }
             composable("completed") { CompletedScreen(taskViewModel) }
             composable("settings") { SettingsScreen(navController, taskViewModel, settingsStore) }
         }
